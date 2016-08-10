79a2b598b4fa7269730be52c7d4314d5865c8e5e
Oskar Niburski <oskarniburski@gmail.com>
2016-08-10 12:09:29 -0400

    Rename

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Faker.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :faker,
+    [app: :faker_elixir,
      version: "0.0.1",
      elixir: "~> 1.3",
      description: "Faker file for creating fake data",
